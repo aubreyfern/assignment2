18098b25848cfe853fc99000c1d01aa22767a31c
Aubrey Fernando <36905039+aubreyfern@users.noreply.github.com>
2019-02-26 21:27:28 -0800

    error checking

--- initialize.py
+++ initialize.py
@@ -4,7 +4,15 @@ try:
     conn  = sqlite3.connect('student.db')
 except Error as e:
     print(e)
-
-#establish cursor & connetion 
+#establish cursor & connetion
 c = conn.cursor()
+conn.commit()
+c.execute("CREATE TABLE IF NOT EXISTS StudentsAssign2("
+                        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                        "FirstName VARCHAR(25),"
+                        "LastName VARCHAR(25),"
+                        "GPA REAL,"
+                        "Major VARCHAR(10),"
+                        "Advisor VARCHAR(25)"
+                        ");")
 conn.commit()
